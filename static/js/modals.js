cf63ec6072c11da109d318bbb250031e822f0ca9
MrTophatt <mohid.mushtaq1@gmail.com>
2024-06-14 15:00:05 -0400

    idk what to call this commit

--- static/js/modals.js
+++ static/js/modals.js
@@ -16,8 +16,10 @@ function showModal(element) {
         element_block.innerHTML = GenerateElementList([], [], null, null) // Filter
         let elements = element_block.querySelectorAll(".element")
         ClickedElement(elements)
+        console.log(document.querySelectorAll(".element-search"))
 
     } else {
+        element--;
         const title = ModalContent.querySelector(".Title")
         const title_name = ModalContent.querySelector(".Title-name")
         const title_num = ModalContent.querySelector(".Title-num")
@@ -32,44 +34,44 @@ function showModal(element) {
         left_arrow.classList.remove(left_arrow.classList[2]?.toString())
         left_arrow.classList.remove(left_arrow.classList[2]?.toString())
         left_arrow.classList.remove(left_arrow.classList[2]?.toString())
-        left_arrow.innerHTML = elementData[element-2]? elementData[element-2].Name : elementData[117].Name
-        left_arrow.classList.add(elementData[element-2]? elementData[element-2].Category.replace(/ /g,"-") : elementData[117].Category.replace(/ /g,"-"))
-        left_arrow.classList.add(elementData[element-2]? (elementData[element-2].ManMade ? "ManMade" : "Natural") : (elementData[117].ManMade ? "ManMade" : "Natural"))
-        left_arrow.classList.add(elementData[element-2]? (elementData[element-2].Diatomic ? "Diatomic" : "Monatomic") : (elementData[117].Diatomic ? "Diatomic" : "Monatomic"))
+        left_arrow.innerHTML = elementData[element-1]? elementData[element-1].Name : elementData[117].Name
+        left_arrow.classList.add(elementData[element-1]? elementData[element-1].Category.replace(/ /g,"-") : elementData[117].Category.replace(/ /g,"-"))
+        left_arrow.classList.add(elementData[element-1]? (elementData[element-1].ManMade ? "ManMade" : "Natural") : (elementData[117].ManMade ? "ManMade" : "Natural"))
+        left_arrow.classList.add(elementData[element-1]? (elementData[element-1].Diatomic ? "Diatomic" : "Monatomic") : (elementData[117].Diatomic ? "Diatomic" : "Monatomic"))
 
         right_arrow.classList.remove(right_arrow.classList[2]?.toString())
         right_arrow.classList.remove(right_arrow.classList[2]?.toString())
         right_arrow.classList.remove(right_arrow.classList[2]?.toString())
-        right_arrow.innerHTML = elementData[element]? elementData[element].Name : elementData[0].Name
-        right_arrow.classList.add(elementData[element]? elementData[element].Category.replace(/ /g,"-") : elementData[0].Category.replace(/ /g,"-"))
-        right_arrow.classList.add(elementData[element]? (elementData[element].ManMade ? "ManMade" : "Natural") : (elementData[0].ManMade ? "ManMade" : "Natural"))
-        right_arrow.classList.add(elementData[element]? (elementData[element].Diatomic ? "Diatomic" : "Monatomic") : (elementData[0].Diatomic ? "Diatomic" : "Monatomic"))
+        right_arrow.innerHTML = elementData[element+1]? elementData[element+1].Name : elementData[0].Name
+        right_arrow.classList.add(elementData[element+1]? elementData[element+1].Category.replace(/ /g,"-") : elementData[0].Category.replace(/ /g,"-"))
+        right_arrow.classList.add(elementData[element+1]? (elementData[element+1].ManMade ? "ManMade" : "Natural") : (elementData[0].ManMade ? "ManMade" : "Natural"))
+        right_arrow.classList.add(elementData[element+1]? (elementData[element+1].Diatomic ? "Diatomic" : "Monatomic") : (elementData[0].Diatomic ? "Diatomic" : "Monatomic"))
 
         title.classList.remove(title.classList[1]?.toString())
         title.classList.remove(title.classList[1]?.toString())
         title.classList.remove(title.classList[1]?.toString())
-        title_name.innerHTML = elementData[element-1].Name
-        title_symbol.innerHTML = elementData[element-1].Symbol
-        title_num.innerHTML = elementData[element-1].AtomicNumber
-        title.classList.add(elementData[element-1].Category.replace(/ /g,"-"))
-        title.classList.add(elementData[element-1].ManMade ? "ManMade" : "Natural")
-        title.classList.add(elementData[element-1].Diatomic ? "Diatomic" : "Monatomic")
+        title_name.innerHTML = elementData[element].Name
+        title_symbol.innerHTML = elementData[element].Symbol
+        title_num.innerHTML = elementData[element].AtomicNumber
+        title.classList.add(elementData[element].Category.replace(/ /g,"-"))
+        title.classList.add(elementData[element].ManMade ? "ManMade" : "Natural")
+        title.classList.add(elementData[element].Diatomic ? "Diatomic" : "Monatomic")
 
         ModalContent.querySelector("#searchInput").style.display = 'none'
         modal.querySelector(".search-container").style.marginBottom = '0'
         ModalContent.querySelector(".basic-info").innerHTML = `
         -- Origin of Name -- <br>
-        ${elementData[element-1].OriginName} <br><br>
+        ${elementData[element].OriginName} <br><br>
         -- Description -- <br>
-        ${elementData[element-1].Description} <br><br>
+        ${elementData[element].Description} <br><br>
         -- Sources -- <br>
-        ${elementData[element-1].Sources} <br><br>
+        ${elementData[element].Sources} <br><br>
         -- Uses -- <br>
-        ${elementData[element-1].Uses} <br><br>
+        ${elementData[element].Uses} <br><br>
         -- Discovery -- <br>
-        Discoverer: ${elementData[element-1].Discovery.People} <br>
-        Location: ${elementData[element-1].Discovery.Location} <br>
-        Year: ${elementData[element-1].Discovery.Year} <br><br>
+        Discoverer: ${elementData[element].Discovery.People} <br>
+        Location: ${elementData[element].Discovery.Location} <br>
+        Year: ${elementData[element].Discovery.Year} <br><br>
         `
     }
 }
@@ -85,6 +87,10 @@ function GenerateElementList(stateOfMatter=[], category=[], manmade=null, diatom
                 <span class='atomic_number'>${x.AtomicNumber}</span>
                 <span class='${x.StateOfMatter} symbol'>${x.Symbol}<br></span>
                 <span class='name'>${x.Name}</span>
+            </div>
+            <div class="element-extraInfo">
+                <span class='atomic_number'>Mass: ${x.Mass}</span>
+                <span class='atomic_number'>Block-${x.Block}</span>
             </div>
         </div>
         `
